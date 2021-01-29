5846cdab43d57e4ef19c57cc64576a7a79d89e2f
“cwpeng-cn” <cw.peng@foxmail.com>
2021-01-29 15:10:16 +0800

    add prid data

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 mkdir storage
-rm -r ../sample_data
-cp -r /content/drive/MyDrive/Colab/ReID\ works/CVPR\ fintuning/net_149.pth ./storage
 mkdir ../../datasets/
+rm -r ../sample_data
+
+cp -r /content/drive/MyDrive/Colab/ReID\ works/CVPR\ fintuning/net_149.pth ./storage
 cp -r /content/drive/MyDrive/科研数据集/prid2011.zip ../../datasets/
-unzip ../../datasets/prid2011.zip ../../datasets
+unzip ../../datasets/prid2011.zip -d ../../datasets
