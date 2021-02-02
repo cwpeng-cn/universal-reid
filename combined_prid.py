5522a4003d3eab2ebd418c42ed6905c495da5765
“cwpeng-cn” <cw.peng@foxmail.com>
2021-02-02 22:03:39 +0800

    add hybrid_net.py

--- combined_prid.py
+++ combined_prid.py
@@ -1,21 +1,23 @@
-import os.path as osp
 from torch.utils.data import Dataset
 from PIL import Image
 import torch
 import os
 import numpy as np
 from torchvision import transforms
+import json
 
-txt_name = "pseudo_label_prid.txt"
+txt_path = "pseudo_label_prid.txt"
 dataset_path = "../../datasets/{}/prid_2011/multi_shot/"
+json_path = "../../datasets/prid2011/splits_prid2011.json"
 gait_dataset_dir = dataset_path.format('prid2011_gait2_cropped')
 rgb_dataset_dir = dataset_path.format("prid2011")
 
 
 class COM_PRID2011(Dataset):
 
-    def __init__(self, seq_num=15):
-        self.tracklets = self.load(txt_name)
+    def __init__(self, seq_num=15, mode="train"):
+        self.train_tracklets = self.load_trainset(txt_path)
+        self.query_tracklets, self.gallery_tracklets = self.load_testset(json_path)
         self.rgb_transform = transforms.Compose([
             transforms.Resize((384, 128)),
             transforms.ToTensor(),
@@ -27,32 +29,71 @@ class COM_PRID2011(Dataset):
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
         self.seq_num = seq_num
+        self.mode = mode
 
     def __getitem__(self, index):
-        cam = "cam_a" if np.random.rand() >= 0.5 else 'cam_b'
+        if self.mode == "train":
+            cam = "cam_a" if np.random.rand() >= 0.5 else 'cam_b'
 
-        rgb_img_paths = np.random.choice(self.tracklets['rgb'][cam][index], size=self.seq_num, replace=True)
-        gait_img_paths = np.random.choice(self.tracklets['gait'][cam][index], size=self.seq_num, replace=True)
+            rgb_img_paths = np.random.choice(self.train_tracklets['rgb'][cam][index], size=self.seq_num, replace=True)
+            gait_img_paths = np.random.choice(self.train_tracklets['gait'][cam][index], size=self.seq_num, replace=True)
 
-        rgb_imgs_data = torch.tensor([])
-        gait_imgs_data = torch.tensor([])
+            rgb_imgs_data = torch.tensor([])
+            gait_imgs_data = torch.tensor([])
 
-        for _ in rgb_img_paths:
-            img_data = Image.open(_).convert('RGB')
-            img = torch.unsqueeze(self.rgb_transform(img_data), 0)
-            rgb_imgs_data = torch.cat((rgb_imgs_data, img), 0)
+            for _ in rgb_img_paths:
+                img_data = Image.open(_).convert('RGB')
+                img = torch.unsqueeze(self.rgb_transform(img_data), 0)
+                rgb_imgs_data = torch.cat((rgb_imgs_data, img), 0)
 
-        for _ in gait_img_paths:
-            img_data = Image.open(_).convert('RGB')
-            img = torch.unsqueeze(self.gait_transform(img_data), 0)
-            gait_imgs_data = torch.cat((gait_imgs_data, img), 0)
+            for _ in gait_img_paths:
+                img_data = Image.open(_).convert('RGB')
+                img = torch.unsqueeze(self.gait_transform(img_data), 0)
+                gait_imgs_data = torch.cat((gait_imgs_data, img), 0)
 
-        return rgb_imgs_data, gait_imgs_data, index
+            return rgb_imgs_data, gait_imgs_data, index
+
+        if self.mode == "query":
+            rgb_imgs_data = torch.tensor([])
+            gait_imgs_data = torch.tensor([])
+
+            for _ in sorted(self.query_tracklets[index]["gait"])[:64]:
+                img_data = Image.open(_).convert('RGB')
+                img = torch.unsqueeze(self.gait_transform(img_data), 0)
+                gait_imgs_data = torch.cat((gait_imgs_data, img), 0)
+
+            for _ in sorted(self.query_tracklets[index]['rgb'])[:64]:
+                img_data = Image.open(_).convert('RGB')
+                img = torch.unsqueeze(self.rgb_transform(img_data), 0)
+                rgb_imgs_data = torch.cat((rgb_imgs_data, img), 0)
+
+            return rgb_imgs_data, gait_imgs_data, index
+
+        if self.mode == "gallery":
+            rgb_imgs_data = torch.tensor([])
+            gait_imgs_data = torch.tensor([])
+
+            for _ in sorted(self.gallery_tracklets[index]["gait"])[:64]:
+                img_data = Image.open(_).convert('RGB')
+                img = torch.unsqueeze(self.gait_transform(img_data), 0)
+                gait_imgs_data = torch.cat((gait_imgs_data, img), 0)
+
+            for _ in sorted(self.gallery_tracklets[index]['rgb'])[:64]:
+                img_data = Image.open(_).convert('RGB')
+                img = torch.unsqueeze(self.rgb_transform(img_data), 0)
+                rgb_imgs_data = torch.cat((rgb_imgs_data, img), 0)
+
+            return rgb_imgs_data, gait_imgs_data, index
 
     def __len__(self):
-        return len(self.tracklets["rgb"]["cam_a"])
+        if self.mode == "train":
+            return len(self.train_tracklets["rgb"]["cam_a"])
+        if self.mode == "query":
+            return len(self.query_tracklets["rgb"])
+        if self.mode == "gallery":
+            return len(self.gallery_tracklets['gait'])
 
-    def load(self, txt_name):
+    def load_trainset(self, txt_name):
         tracklets = {"rgb": {'cam_a': [], 'cam_b': []}, "gait": {'cam_a': [], 'cam_b': []}}
         with open(txt_name, 'r') as f:
             lines = f.readlines()
@@ -75,3 +116,25 @@ class COM_PRID2011(Dataset):
                     tracklets['rgb'][cam].append(rgb_tracklet)
                     tracklets['gait'][cam].append(gait_tracklet)
         return tracklets
+
+    def load_testset(self, json_path):
+        with open(json_path, "r") as f:
+            persons = json.load(f)[0]
+            test_persons = persons['test']
+            query_tracklets = []
+            gallery_tracklets = []
+            for person in test_persons:
+                query_rgb_tracklet, query_gait_tracklet, gallery_rgb_tracklet, gallery_gait_tracklet = [], [], [], []
+
+                for img_name in os.listdir(gait_dataset_dir + "cam_a/" + person):
+                    if img_name.endswith("png"):
+                        query_rgb_tracklet.append(rgb_dataset_dir + "cam_a/" + person + "/" + img_name)
+                        query_gait_tracklet.append(gait_dataset_dir + "cam_a/" + person + img_name)
+
+                for img_name in os.listdir(gait_dataset_dir + "cam_b/" + person):
+                    if img_name.endswith("png"):
+                        gallery_rgb_tracklet.append(rgb_dataset_dir + "cam_b/" + person + img_name)
+                        gallery_gait_tracklet.append(gait_dataset_dir + "cam_b/" + img_name)
+                query_tracklets.append({"gait": query_gait_tracklet, "rgb": query_rgb_tracklet})
+                gallery_tracklets.append({"gait": gallery_gait_tracklet, "rgb": gallery_rgb_tracklet})
+            return query_tracklets, gallery_tracklets
---
+++ train.py
@@ -0,0 +1,43 @@
+from combined_prid import COM_PRID2011
+import numpy as np
+from reid.data import transforms
+from reid.data.samplers import RandomIdentitySampler
+from torch.utils.data import DataLoader
+from reid.utils.model_save_restore import *
+from reid.evaluation import market_evaluate
+from torch.utils.data import Dataset
+import csv
+from PIL import Image
+from torch import nn
+from torch.nn import functional as F
+from torch.nn import init
+from reid.data.prid2011 import PRID2011
+import reid.feature_op_video as FO
+from rgnet import RGNet
+from torch import optim
+
+EPOCH = 30
+SEQ_NUM = 10
+NUM_CLASS = 144
+
+train_dataset = COM_PRID2011(seq_num=SEQ_NUM, mode="train")
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=False)
+
+query_dataset = COM_PRID2011(mode="query")
+query_loader = DataLoader(query_dataset, batch_size=1, shuffle=False)
+gallery_dataset = COM_PRID2011(mode="gallery")
+gallery_loader = DataLoader(gallery_dataset, batch_size=1, shuffle=False)
+
+net = RGNet(num_class=NUM_CLASS, seq_num=SEQ_NUM)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(list(net.gait_net.parameters()) + list(net.classifier.parameters()) + list(net.fc.parameters()))
+
+for epoch in range(EPOCH):
+    for data in train_loader:
+        rgb_seqs, gait_seqs, ids = data
+        output = net(rgb_seqs.cuda(), gait_seqs.cuda())
+        loss = criterion(output, ids.cuda())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(loss.item())
