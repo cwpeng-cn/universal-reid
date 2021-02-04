a49efe4c0b8054c3d3991ff7baeebdd872397736
“cwpeng-cn” <cw.peng@foxmail.com>
2021-02-04 15:04:22 +0800

    add train_new.py

---
+++ rgnet_new.py
@@ -0,0 +1,90 @@
+from reid.model.pgs_new import PGSNet
+from reid.utils.model_save_restore import *
+from torch import nn
+from gait.model.resnet_video import resnet50 as gaitnet
+
+
+class RGNet(nn.Module):
+    def __init__(self, num_class, seq_num):
+        super(RGNet, self).__init__()
+        self.seq_num = seq_num
+        rgb_net = PGSNet(num_classes=num_class, num_features=1024)
+        self.rgb_net = restore_network("./", 9, rgb_net).eval()
+        self.gait_net = gaitnet(num_classes=num_class, pretrained=True, seq_num=seq_num, droprate=0.1)
+        self.fc = self._construct_fc_layer([1024], 4096, 0.1)
+        self.classifier = nn.Linear(1024, num_class)
+        self._init_params(self.fc)
+        self._init_params(self.classifier)
+
+    def forward(self, rgb_seqs, gait_seqs):
+        n, s, c, h1, w1 = rgb_seqs.shape
+        rgb_seqs = rgb_seqs.view(n * s, c, h1, w1)
+        n, s, c, h2, w2 = gait_seqs.shape
+        gait_seqs = gait_seqs.view(n * s, c, h2, w2)
+        rgb_features = torch.mean(self.rgb_net(rgb_seqs).view(n, s, -1), 1)
+        gait_features = self.gait_net(gait_seqs)
+        combined = torch.cat([rgb_features, gait_features], 1)
+        features = self.fc(combined)
+        if not self.training:
+            return features
+        else:
+            return self.classifier(features)
+
+    def _construct_fc_layer(self, fc_dims, input_dim, dropout_p=None):
+        """Constructs fully connected layer
+
+        Args:
+            fc_dims (list or tuple): dimensions of fc layers, if None, no fc layers are constructed
+            input_dim (int): input dimension
+            dropout_p (float): dropout probability, if None, dropout is unused
+        """
+        if fc_dims is None:
+            self.feature_dim = input_dim
+            return None
+
+        assert isinstance(
+            fc_dims, (list, tuple)
+        ), 'fc_dims must be either list or tuple, but got {}'.format(
+            type(fc_dims)
+        )
+
+        layers = []
+        for dim in fc_dims:
+            layers.append(nn.Linear(input_dim, dim))
+            layers.append(nn.BatchNorm1d(dim))
+            layers.append(nn.ReLU(inplace=True))
+            if dropout_p is not None:
+                layers.append(nn.Dropout(p=dropout_p))
+            input_dim = dim
+
+        self.feature_dim = fc_dims[-1]
+
+        return nn.Sequential(*layers)
+
+    def _init_params(self, m):
+        if isinstance(m, nn.Conv2d):
+            nn.init.kaiming_normal_(
+                m.weight, mode='fan_out', nonlinearity='relu'
+            )
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.BatchNorm2d):
+            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.BatchNorm1d):
+            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.Linear):
+            nn.init.normal_(m.weight, 0, 0.01)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def normalize(self, x, axis=-1):
+        """Normalizing to unit length along the specified dimension.
+        Args:
+          x: pytorch Variable
+        Returns:
+          x: pytorch Variable, same shape as input
+        """
+        x = 1. * x / (torch.norm(x, 2, axis, keepdim=True).expand_as(x) + 1e-12)
+        return x
---
+++ train_new.py
@@ -0,0 +1,65 @@
+from combined_prid import COM_PRID2011
+import numpy as np
+from reid.data import transforms
+from reid.data.samplers import RandomIdentitySampler
+from torch.utils.data import DataLoader
+from reid.utils.model_save_restore import *
+from reid.evaluation import market_evaluate
+from torch.utils.data import Dataset
+import csv
+from PIL import Image
+from torch import nn
+from torch.nn import functional as F
+from torch.nn import init
+from reid.data.prid2011 import PRID2011
+import reid.feature_op_video as FO
+from rgnet_new import RGNet
+from torch import optim
+
+EPOCH = 30
+SEQ_NUM = 10
+NUM_CLASS = 144
+
+train_dataset = COM_PRID2011(seq_num=SEQ_NUM, mode="train")
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+
+query_dataset = COM_PRID2011(mode="query")
+query_loader = DataLoader(query_dataset, batch_size=1, shuffle=False)
+gallery_dataset = COM_PRID2011(mode="gallery")
+gallery_loader = DataLoader(gallery_dataset, batch_size=1, shuffle=False)
+
+net = RGNet(num_class=NUM_CLASS, seq_num=SEQ_NUM).cuda()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(
+    list(net.gait_net.parameters()) + list(net.classifier.parameters()) + list(net.fc.parameters()) + list(
+        net.rgb_net.parameters()),
+    lr=0.001)
+
+print("测试直接迁移结果")
+query_feature, query_id, query_camera = FO.extract_cnn_feature_combined(net, loader=query_loader, vis=True,
+                                                                        mode="query")
+gallery_feature, gallery_id, gallery_camera = FO.extract_cnn_feature_combined(net, loader=gallery_loader, vis=True,
+                                                                              mode="gallery")
+map, cmc = market_evaluate.evaluate(query_feature, np.array(query_id), np.array(query_camera), gallery_feature,
+                                    np.array(gallery_id), np.array(gallery_camera), vis=False)
+print("直接迁移结果: rank-1:{},rank-5:{},rank-10:{},rank-20:{}".format(cmc[0], cmc[4], cmc[9], cmc[19]))
+for epoch in range(EPOCH):
+    for step, data in enumerate(train_loader):
+        rgb_seqs, gait_seqs, ids = data
+        output = net(rgb_seqs.cuda(), gait_seqs.cuda())
+        loss = criterion(output, ids.cuda())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if step % 10 == 0:
+            print(loss.item())
+    if epoch % 2 == 0:
+        print("第{}轮效果评估开始>>>".format(epoch + 1))
+        query_feature, query_id, query_camera = FO.extract_cnn_feature_combined(net, loader=query_loader, vis=False,
+                                                                                mode="query")
+        gallery_feature, gallery_id, gallery_camera = FO.extract_cnn_feature_combined(net, loader=gallery_loader,
+                                                                                      vis=False, mode="gallery")
+        map, cmc = market_evaluate.evaluate(query_feature, np.array(query_id), np.array(query_camera), gallery_feature,
+                                            np.array(gallery_id), np.array(gallery_camera), vis=False)
+        print("第{}轮训练结果: rank-1:{},rank-5:{},rank-10:{},rank-20:{}".format(epoch + 1, cmc[0], cmc[4], cmc[9],
+                                                                           cmc[19]))
