65ae2d3d389b5e3727281868cf5454386117ec90
“cwpeng-cn” <cw.peng@foxmail.com>
2021-02-04 15:36:22 +0800

    add train_new.py

--- train_new.py
+++ train_new.py
@@ -19,6 +19,7 @@ from torch import optim
 EPOCH = 30
 SEQ_NUM = 10
 NUM_CLASS = 144
+LR = 0.001
 
 train_dataset = COM_PRID2011(seq_num=SEQ_NUM, mode="train")
 train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
@@ -29,11 +30,14 @@ gallery_dataset = COM_PRID2011(mode="gallery")
 gallery_loader = DataLoader(gallery_dataset, batch_size=1, shuffle=False)
 
 net = RGNet(num_class=NUM_CLASS, seq_num=SEQ_NUM).cuda()
+
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(
-    list(net.gait_net.parameters()) + list(net.classifier.parameters()) + list(net.fc.parameters()) + list(
-        net.rgb_net.parameters()),
-    lr=0.001)
+    [{"params": list(net.gait_net.parameters()) + list(net.classifier.parameters()) + list(net.fc.parameters())}],
+    lr=LR)
+optimizer2 = optim.Adam(
+    [{"params": list(net.gait_net.parameters()) + list(net.classifier.parameters()) + list(net.fc.parameters())},
+     {"params": net.rgb_net.parameters(), "lr": LR / 10}], lr=LR)
 
 print("测试直接迁移结果")
 query_feature, query_id, query_camera = FO.extract_cnn_feature_combined(net, loader=query_loader, vis=True,
@@ -44,6 +48,8 @@ map, cmc = market_evaluate.evaluate(query_feature, np.array(query_id), np.array(
                                     np.array(gallery_id), np.array(gallery_camera), vis=False)
 print("直接迁移结果: rank-1:{},rank-5:{},rank-10:{},rank-20:{}".format(cmc[0], cmc[4], cmc[9], cmc[19]))
 for epoch in range(EPOCH):
+    if epoch == 10:
+        optimizer = optimizer2
     for step, data in enumerate(train_loader):
         rgb_seqs, gait_seqs, ids = data
         output = net(rgb_seqs.cuda(), gait_seqs.cuda())
